b55fa6356690e4d2d7797e890a8aa4b1406340fb
Мурзоев Мизбоншох Содирович <msmurzoev@edu.hse.ru>
2025-02-27 23:49:47 +0500

    Версия1.0.0

---
+++ shop/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+
+from .models import Category, Product
+
+
+
+class Index(ListView):
+    """Главная страница"""
+    model = Product
+    extra_context = {"title": "Главная страница"}
+    template_name = "shop/index.html"
+
+
+
